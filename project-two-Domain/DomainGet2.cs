daad87ca3e9d5ed1becdcc4de4992c70cfb74076
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-25 14:25:12 +0500

    adad

---
+++ project-two-Domain/DomainGet2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_two_Domain
+{
+    public class DomainGet2
+    {
+        public string Name { get; set; } = "DomainGet2";
+    }
+}
